8844b53045832b7f3362c971e8d06de61b914069
Jordan Lumley <jordie1966@hotmail.co.uk>
2023-06-26 15:52:58 +0100

    commit after ex 11

---
+++ ex_11/ex11_starter.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python
+import re
+
+Belgium = 'Belgium,10445852,Brussels,737966,' \
+          'Europe,1830,Euro,Catholicism,Dutch,French,German'
+print(Belgium)
+
+print('')
+print('')
+
+# Task 3a
+# Finds length of belgium and prints a hyphen for every character in the string.
+belgium_length = len(Belgium)
+print('-'*belgium_length)
+
+print('')
+print('')
+
+# Replaces all instances of the first with the second.
+# Can this be changed to replace all alpha and numerical?
+print(Belgium.replace('o', '-'))
+
+print('')
+print('')
+
+# Task 3b
+# Replace all commas with a colon.
+print(Belgium.replace(',', ':'))
+
+print('')
+print('')
+
+# Task 3c
+# Add the population of Belgium and Brussels together.
+# Populations must be changed from a string to an integer.
+
+Belgium_population = int(Belgium[8:16])
+Brussels_population = int(Belgium[26:32])
+BelgandBruss_population = (Belgium_population + Brussels_population)
+print(BelgandBruss_population)
+
